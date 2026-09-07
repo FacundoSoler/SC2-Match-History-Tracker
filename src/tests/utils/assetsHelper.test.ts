2cfcf91dd49942e81a1b04c7bed441adb0d062e5
FacundoSoler <64513528+FacundoSoler@users.noreply.github.com>
2026-09-07 20:02:23 -0300

    UNIT TESTS: Added remaining use cases for most played race games
    UNIT TESTS: Added remaining use cases for most played race games

--- src/tests/utils/assetsHelper.test.ts
+++ src/tests/utils/assetsHelper.test.ts
@@ -30,4 +30,34 @@ describe('LinkFormatter', () => {
 
         expect(result).toBe(expected);
     });
+
+    it('setRaceIconLink returns valid most played race icon for Terran', () =>{
+        debugger;
+        const arrange: RaceGames = {
+            PROTOSS: 750,
+            TERRAN: 1000,
+            ZERG: 900,
+            RANDOM: 500
+        };
+
+        const result = getRaceIconLink(arrange);
+        const expected = '/assets/terran.svg';
+
+        expect(result).toBe(expected);
+    });
+
+    it('setRaceIconLink returns valid most played race icon for Random', () =>{
+        debugger;
+        const arrange: RaceGames = {
+            PROTOSS: 750,
+            TERRAN: 50,
+            ZERG: 900,
+            RANDOM: 1200
+        };
+
+        const result = getRaceIconLink(arrange);
+        const expected = '/assets/random.svg';
+
+        expect(result).toBe(expected);
+    });
 })
